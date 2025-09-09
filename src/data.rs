3f0258793cf7352f92d76e1884f705183dc958a5
Liu Xiaoyi <circuitcoder0@gmail.com>
2025-09-10 03:59:03 +0800

    Parse dates and bookmark tags

--- src/data.rs
+++ src/data.rs
@@ -13,6 +13,37 @@ where
     s.parse::<u64>().map_err(serde::de::Error::custom)
 }
 
+fn de_str_or_u64_to_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    struct StrOrU64;
+
+    impl<'de> serde::de::Visitor<'de> for StrOrU64 {
+        type Value = u64;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("string or u64")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            v.parse::<u64>().map_err(serde::de::Error::custom)
+        }
+
+        fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            Ok(v)
+        }
+    }
+
+    deserializer.deserialize_any(StrOrU64)
+}
+
 #[derive(Deserialize_repr)]
 #[repr(u8)]
 pub enum XRestrict {
@@ -38,18 +69,20 @@ pub struct BookmarkData {
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BookmarkedWork {
-    #[serde(deserialize_with = "de_str_to_u64")]
+    #[serde(deserialize_with = "de_str_or_u64_to_u64")]
     id: u64,
     title: String,
     tags: Vec<String>,
     x_restrict: XRestrict,
-    restruct: u8, // TODO: figure out what is this
+    restrict: u8, // TODO: figure out what is this
 
-    #[serde(deserialize_with = "de_str_to_u64")]
+    #[serde(deserialize_with = "de_str_or_u64_to_u64")]
     user_id: u64,
     user_name: String,
 
     bookmark_data: BookmarkData,
+    create_date: chrono::DateTime<chrono::FixedOffset>,
+    update_date: chrono::DateTime<chrono::FixedOffset>,
 
     width: u64,
     height: u64,
@@ -63,7 +96,39 @@ pub struct BookmarkedWork {
 pub struct Bookmarks {
     pub total: usize,
     pub works: Vec<BookmarkedWork>,
-    pub bookmark_tags: HashMap<String, Vec<String>>,
+    pub bookmark_tags: HashMap<u64, Vec<String>>,
+}
+
+impl Bookmarks {
+    pub fn into_illusts(&mut self) -> impl Iterator<Item = Illust> {
+        let mut tags_map = std::mem::take(&mut self.bookmark_tags);
+        self.works.drain(..).map(move |work: BookmarkedWork| {
+            let state = if work.is_unlisted {
+                IllustState::Unlisted
+            } else if work.is_masked {
+                IllustState::Masked
+            } else {
+                IllustState::Normal
+            };
+            let data = IllustData::Fetched {
+                title: work.title,
+                tags: work.tags,
+                user: Illustrator {
+                    id: work.user_id,
+                    name: work.user_name,
+                },
+                create_date: work.create_date,
+                update_date: work.update_date,
+                x_restrict: work.x_restrict,
+            };
+            let bookmarked_tags = tags_map.remove(&work.id).unwrap();
+            Illust {
+                data,
+                state,
+                bookmarked_tags,
+            }
+        })
+    }
 }
 
 #[derive(Deserialize)]
@@ -104,4 +169,35 @@ pub async fn get_bookmarks_page(user: User, tag: Option<&str>, hidden: bool, off
     let resp = client.execute(req).await?;
     let json: Response<Bookmarks> = resp.json().await?;
     json.into_body()
+}
+
+// Parsed data
+
+pub enum IllustState {
+    Normal,
+    Unlisted,
+    Masked,
+}
+
+pub struct Illustrator {
+    id: u64,
+    name: String,
+}
+
+pub enum IllustData {
+    Unknown,
+    Fetched {
+        title: String,
+        tags: Vec<String>,
+        user: Illustrator,
+        create_date: chrono::DateTime<chrono::FixedOffset>,
+        update_date: chrono::DateTime<chrono::FixedOffset>,
+        x_restrict: XRestrict,
+    }
+}
+
+pub struct Illust {
+    data: IllustData,
+    state: IllustState,
+    bookmarked_tags: Vec<String>,
 }

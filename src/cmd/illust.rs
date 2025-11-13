5d19a6271fc2865d75f5458eb95ecb026eff1ba3
Liu Xiaoyi <circuitcoder0@gmail.com>
2025-11-13 23:12:54 +0800

    Allow to sync list of illusts

--- src/cmd/illust.rs
+++ src/cmd/illust.rs
@@ -2,26 +2,68 @@ use std::collections::HashMap;
 
 use clap::Args;
 
+use crate::util::DownloadIdSrc;
+
 #[derive(Args)]
 pub struct Illust {
-    /// Illustration ID
-    id: u64,
+    #[clap(flatten)]
+    /// ID of the illustration
+    id: DownloadIdSrc<u64>,
 
     /// Dry run, only fech and print the info
     #[arg(long)]
     dry_run: bool,
+
+    /// Abort if failed
+    #[arg(long)]
+    abort_on_fail: bool,
 }
 
 impl Illust {
     pub async fn run(self, session: &crate::config::Session) -> anyhow::Result<()> {
-        let illust = crate::data::pixiv::get_illust(session, self.id).await?;
-        if self.dry_run {
-            tracing::info!("Fetched: {:?}", illust);
-            return Ok(());
+        let mut errored = 0;
+        for id in self.id.read()? {
+            let id = id?;
+            let ret = self.sync_single(session, id).await;
+            if ret.is_err() {
+                if self.abort_on_fail {
+                    return ret;
+                } else {
+                    tracing::error!("Failed to sync illust {}: {:?}", id, ret.err());
+                    errored += 1;
+                }
+            } else {
+            }
         }
 
-        let mut tag_map_ctx: HashMap<String, u64> = HashMap::new();
-        crate::db::update_illust(&illust, &mut tag_map_ctx).await?;
+        if errored == 0 {
+            Ok(())
+        } else {
+            return Err(anyhow::anyhow!("{} illust(s) failed to sync", errored));
+        }
+    }
+
+    async fn sync_single(&self, session: &crate::config::Session, id: u64) -> anyhow::Result<()> {
+        let illust = crate::data::pixiv::get_illust(session, id).await?;
+
+        if !self.dry_run {
+            let mut tag_map_ctx: HashMap<String, u64> = HashMap::new();
+            let update_result = crate::db::update_illust(&illust, &mut tag_map_ctx).await?;
+            let update_prompt = match update_result {
+                crate::db::IllustUpdateResult::Inserted => "INSERTED",
+                crate::db::IllustUpdateResult::BookmarkIDChanged => "BMIDCHANGED",
+                crate::db::IllustUpdateResult::Updated => "UPDATED",
+                crate::db::IllustUpdateResult::Skipped => "SKIPPED",
+            };
+            tracing::info!(
+                "Synced {}: [{}] {}",
+                illust.id,
+                update_prompt,
+                illust.data.display_title()
+            );
+        } else {
+            tracing::info!("Fetched {}: {}", illust.id, illust.data.display_title());
+        }
         Ok(())
     }
 }
